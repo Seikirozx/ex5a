602dc14c7cd3d1d30c6a98ccdf84df7a2e0499fc
Lalita <pg289378@gmail.com>
2025-02-05 10:57:44 +0700

    refactor#2 adding validate fn.

--- common.js
+++ common.js
@@ -8,6 +8,15 @@ function validate(pace){
 		return {"status":0,"message":msg,"hour":0,"min":0};
 	}
 }
+function paceCal(){
+	var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+		}
+	return {"min":min,hour,"hour":hour};	
+}
 
 function calculate(p,d){
 		var msg = "";
@@ -17,12 +26,7 @@ function calculate(p,d){
 		var chk = validate(pace);
 		if(chk != null) return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
